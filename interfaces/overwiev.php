b63ea8b919303074910e6291f5f1d15075f4dc76
Pappy33 <pat.rumanovska@gmail.com>
2016-09-18 10:20:49 +0200

    Add files via upload
    overwiev page

---
+++ interfaces/overwiev.php
@@ -0,0 +1,311 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Logo Nav - Start Bootstrap Template</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/logo-nav.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">
+                  <p style="white"><img src="img/paper-airplane-icon.png" alt="businesso_logo">Businesso</p>
+
+                </a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+             <!--    <ul class="nav navbar-nav">
+                    <li>
+                        <a href="#">Offers</a>
+                    </li>
+                    <li>
+                        <a href="#">Services</a>
+                    </li>
+                    <li>
+                        <a href="#">Contact</a>
+                    </li>
+                </ul> -->
+                <ul class="nav navbar-nav navbar-right">
+                  <li><a href="#"><span class="glyphicon glyphicon-user"></span> John Mauyer</a></li>
+                  <li><a href="#"><span class="glyphicon glyphicon-chevron-down"></span></a></li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
+    <!-- Page Content -->
+    <div class="container" id="overwiev">
+        <div class="row">
+            <div class="col-lg-12">
+                <div id="history">
+                    <a href="#"> Search </a><span class="glyphicon glyphicon-chevron-right"></span>
+                    <a href="#"> Select flight </a><span class="glyphicon glyphicon-chevron-right"></span>
+                    <a href="#"> Overwiev </a>
+                </div>
+            </div>
+        </div>
+
+            
+
+            <!-- DEPARTURES-->
+            <div class="row">
+            <div class="col-lg-12 separator"></div>
+            <div class="col-lg-12 text-right" id="edit">
+                <p>Edit</p>
+            </div>
+                <div class="col-lg-12" id="blue">
+
+                    <div class="col-lg-1">
+                        <img src="img/departures.png" alt="plane_logo_departure">
+                    </div>
+
+                    <div class="col-lg-3">
+                        <p>CPH - BCN</p>
+                    </div>
+
+                    <div class="col-lg-2 col-lg-offset-6">
+                        <p>225 Dkk</p>
+                    </div>
+
+                </div>
+            </div>
+
+            <div class="row" id="grey">
+                    <div class="col-lg-10 col-lg-offset-1">
+                       <div class="col-lg-3">
+                            <div>
+                                <img src="img/calendar.png" alt="calendar_logo"/><span>Departure: 9/10/2016 </span> 
+                            </div>
+                        </div>
+
+                        <div class="col-lg-3">
+                            <div>
+                                <img src="img/clock.png" alt="clock_logo"/><span> 12:35 </span> 
+                            </div>
+                        </div>
+
+                        <div class="col-lg-3">
+                            <div>
+                                <img src="img/cutlery.png" alt="cutlery_logo"/><span> Vegetarian food </span> <br/>
+                            </div>
+                        </div>
+                    </div>               
+            </div>
+
+            <div class="row" id="grey">
+                    <div class="col-lg-10 col-lg-offset-1">
+                       <div class="col-lg-3">
+                            <div>
+                                <img src="img/calendar.png" alt="calendar_logo"/><span>Ariival 9/10/2016 </span> 
+                            </div>
+                        </div>
+
+                        <div class="col-lg-3">
+                            <div>
+                                <img src="img/clock.png" alt="clock_logo"/><span> 14:35 </span> 
+                            </div>
+                        </div>
+                    </div>               
+            </div>    
+            <!-- END OF DEPARTURES -->
+
+            <div class="col-lg-12 flight_separator"></div>
+
+            <!-- ARRIVALS-->
+            <div class="row">
+                <div class="col-lg-12" id="blue">
+                    <div class="col-lg-1">
+                        <img src="img/arrival.png" alt="plane_logo_arrival">
+                    </div>
+
+                    <div class="col-lg-3">
+                        <p>CPH - BCN</p>
+                    </div>
+
+                    <div class="col-lg-2 col-lg-offset-6">
+                        <p>225 Dkk</p>
+                    </div>
+
+                </div>
+            </div>
+
+            <div class="row" id="grey">
+                    <div class="col-lg-10 col-lg-offset-1">
+                       <div class="col-lg-3">
+                            <div>
+                                <img src="img/calendar.png" alt="calendar_logo"/><span>Departure: 14/10/2016 </span> 
+                            </div>
+                        </div>
+
+                        <div class="col-lg-3">
+                            <div>
+                                <img src="img/clock.png" alt="clock_logo"/><span> 10:35 </span> 
+                            </div>
+                        </div>
+
+                        <div class="col-lg-3">
+                            <div>
+                                <img src="img/cutlery.png" alt="cutlery_logo"/><span> Vegetarian food </span> 
+                            </div>
+                        </div>
+                    </div>               
+            </div>
+
+             <div class="row" id="grey">
+                    <div class="col-lg-10 col-lg-offset-1">
+                       <div class="col-lg-3">
+                            <div>
+                                <img src="img/calendar.png" alt="calendar_logo"/><span>Arrival: 14/10/2016 </span> 
+                            </div>
+                        </div>
+
+                        <div class="col-lg-3">
+                            <div>
+                                <img src="img/clock.png" alt="clock_logo"/><span> 12:35 </span> 
+                            </div>
+                        </div>
+                    </div>               
+            </div>        
+            <!-- END OF ARRIVALS -->
+
+            <div class="col-lg-12 flight_separator"></div>
+
+            <!-- ACCOMMODATION -->
+            <div class="row" >
+                <div class="col-lg-12" id="blue">
+                    <div class="col-lg-1">
+                        <img src="img/house-outline.png" alt="accommodation_logo">
+                    </div>
+
+                    <div class="col-lg-3">
+                        <p>Barcelona city hostel * </p>
+                    </div>
+
+                    <div class="col-lg-2 col-lg-offset-6">
+                        <p>525 Dkk</p>
+                    </div>
+
+                </div>
+            </div>
+
+            <div class="row" id="grey">
+                    <div class="col-lg-10 col-lg-offset-1">
+                       <div class="col-lg-3">
+                            <div>
+                                <img src="img/calendar.png" alt="calendar_logo"/><span>Check-in: 9/10/2016 </span> 
+                            </div>
+                        </div>
+
+                        <div class="col-lg-3">
+                            <div>
+                                <img src="img/clock.png" alt="clock_logo"/><span> 12:35 </span> 
+                            </div>
+                        </div>
+                    </div>               
+            </div>
+
+            <div class="row" id="grey">
+                    <div class="col-lg-10 col-lg-offset-1">
+                       <div class="col-lg-3">
+                            <div>
+                                <img src="img/calendar.png" alt="calendar_logo"/><span>Check-out 14/10/2016 </span> 
+                            </div>
+                        </div>
+
+                        <div class="col-lg-3">
+                            <div>
+                                <img src="img/clock.png" alt="clock_logo"/><span> 08:30 </span> 
+                            </div>
+                        </div>
+                    </div>               
+            </div>  
+            <div class="col-lg-12">
+                <hr/>
+            </div> 
+<div class="row">
+            <div class="col-lg-12">
+                <div class="col-lg-3 col-lg-offset-1">
+                <p>Total</p>
+                </div>
+                <div class="col-lg-2 col-lg-offset-6">
+                <p>1025 Dkk</p>
+                </div>
+            </div>  
+</div> 
+<div class="row">
+<div class="col-lg-12">
+<div class="col-lg-3 col-lg-offset-4">
+<label class="checkbox-inline"><input type="checkbox" value="active">Use register card</label>
+</div>
+<div class="col-lg-3 ">
+<label class="checkbox-inline"><input type="checkbox" value="">Paypal</label>
+</div>
+
+<div class="col-lg-12 flight_separator"></div>
+
+</div>
+</div>
+<div class="col-lg-4  col-lg-offset-5">
+<button class="btn btn-primary">Proceed to payment</button>
+</div>
+<div class="col-lg-12 flight_separator"></div>
+        
+        
+    </div>
+    <!-- /.container -->
+
+<footer>
+<div>
+    <div class="panel-footer">
+    <p style="text-align:center">Copyright © <?php echo date("Y"); ?> Businesso, Copenhagen</p> 
+        <div id="footer"><a href="#"> Contact </a>|<a href="#"> FAQ </a>|<a href="#"> About </a></div>
+
+    </div>
+
+</div>
+</footer>
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
