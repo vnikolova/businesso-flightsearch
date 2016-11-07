332e79853fdf04afc2579b46dac0075dd5ad2e7c
kasiap19 <kasiap19@gmail.com>
2016-11-07 22:47:39 +0100

    before it didnt work for one way but i fixed it

--- js/script.js
+++ js/script.js
@@ -65,23 +65,64 @@
       }
     });
 
-  $(function() {
-
-      // switch for one way
-    $("#returnOn").on("click",function(e) {
-      e.preventDefault();
+    // oneWayBtn
+    $(document).on("click", "#onewayBtn", function(){
       $("#return").hide();
       $("#oneWay").show();
     });
-    // switch for return
-    $("#returnOff").on("click",function(e) {
-      e.preventDefault();
-      $("#return").show();
+
+    // returnBtn
+    $(document).on("click", "#returnBtn", function(){
       $("#oneWay").hide();
+      $("#return").show();
     });
 
+    // searc btn return
+    $(document).on("click", "#searchBtnReturn", function(){
+      console.log("x");
+      // take values
+      var departureDes = $("#departureDes").val();
+      var arrivalDes = $("#arrivalDes").val();
+      var departureDate = $("#depart").val();
+      var arrivalDate = $("#arriveDate").val();
+      var passangersnumber = $("#passNumber").val();
 
-  });
+      var linkToApi = 'api.php?command=return&departureDes='+departureDes+'&arrivalDes='+arrivalDes+'&departureDate='+departureDate+'&arrivalDate='+arrivalDate+'&passangersnumber='+passangersnumber;
+      console.log(linkToApi);
+      $.getJSON(linkToApi, function(jData){
+        // console.log(jData);
+          });
+
+      $("#content").load("results.html");
+    });
+    // search btn one way
+    $(document).on("click", "#searchBtnOneway", function(){
+      // take values
+      var departureDesOne = $("#departureDesOne").val();
+      var arrivalDesOne = $("#arrivalDesOne").val();
+      var departOne = $("#departOne").val();
+      var passNumberOne = $("#passNumberOne").val();
+
+      var linktoApi = 'api.php?command=oneway&departureDesOne='+departureDesOne+'&arrivalDesOne='+arrivalDesOne+'&departOne='+departOne+'&passNumberOne='+passNumberOne; 
+      console.log(linktoApi);
+      $.getJSON(linktoApi, function(jData){
+        // console.log(jData);
+          });
+      $("#content").load("results.html");
+    });
+
+    // datepicker
+  $(function () {
+    // departure date picker
+    $('#depart').datepicker(); // departure date picker
+    $('#arriveDate').datepicker();
+    // arrival date picker
+    $('#departOne').datepicker();
+    })
+
+
+
+  // });
   // //display values from localhost 
   // document.getElementById("displayedUser").innerHTML = localStorage.getItem("name");
   // document.getElementById("displayedName").innerHTML = localStorage.getItem("name");
@@ -99,24 +140,6 @@
 
   });
 
-  // search btn
-  $(document).on("click", "#searchBtn", function(){
-     //window.open("results.html")
-    // values from inputs
-    var departureDes = $("#departureDes").val();
-    var arrivalDes = $("#arrivalDes").val();
-    var departDate = $("#depart").val();
-    var arriveDate = $("#arriveDate").val();
-    var passangers = $('#passNumber :selected').text();
-    // link to api
-    var linkToApi = 'api.php?command=save&departureDes='+departureDes+'&arrivalDes='+arrivalDes+'&departDate='+departDate+'&arriveDate='+arriveDate+'&passangers='+passangers;
-    console.log(linkToApi);
-    $.getJSON(linkToApi, function(jData){
-        console.log(jData);
-          });
-
-
-  });
   $(document).on("click", ".btn-select", function(){
      window.location.assign("overview.html");
   });
@@ -149,105 +172,105 @@
       $('.alert').show().html('You selected <strong>' + val + '</strong>: <strong>' + text + '</strong>');
   }
 
-  $(function () {
+  // $(function () {
 
-      $('#demo1').typeahead({
-          source: [
-          { id: 1, name: 'Toronto' },
-          { id: 2, name: 'Montreal' },
-          { id: 3, name: 'New York' },
-          { id: 4, name: 'Buffalo' },
-          { id: 5, name: 'Boston' },
-          { id: 6, name: 'Columbus' },
-          { id: 7, name: 'Dallas' },
-          { id: 8, name: 'Vancouver' },
-          { id: 9, name: 'Seattle' },
-          { id: 10, name: 'Los Angeles' },
-          { id: 11, name: 'Copenhagen' },
-          { id: 12, name: 'Berlin' },
-          { id: 13, name: 'Las Vegas' },
-          { id: 14, name: 'Barcelona' },
-          { id: 15, name: 'London' },
-          { id: 16, name: 'Paris' },
-          { id: 17, name: 'Oslo' },
-          { id: 18, name: 'Malmo' }
-        ],
-          itemSelected: displayResult
-      });
-      $('#demo2').typeahead({
-          source: [
-          { id: 1, name: 'Toronto' },
-          { id: 2, name: 'Montreal' },
-          { id: 3, name: 'New York' },
-          { id: 4, name: 'Buffalo' },
-          { id: 5, name: 'Boston' },
-          { id: 6, name: 'Columbus' },
-          { id: 7, name: 'Dallas' },
-          { id: 8, name: 'Vancouver' },
-          { id: 9, name: 'Seattle' },
-          { id: 10, name: 'Los Angeles' },
-          { id: 11, name: 'Copenhagen' },
-          { id: 12, name: 'Berlin' },
-          { id: 13, name: 'Las Vegas' },
-          { id: 14, name: 'Barcelona' },
-          { id: 15, name: 'London' },
-          { id: 16, name: 'Paris' },
-          { id: 17, name: 'Oslo' },
-          { id: 18, name: 'Malmo' }
-        ],
-          itemSelected: displayResult
-      });
-      $('#demo3').typeahead({
-          source: [
-          { id: 1, name: 'Toronto' },
-          { id: 2, name: 'Montreal' },
-          { id: 3, name: 'New York' },
-          { id: 4, name: 'Buffalo' },
-          { id: 5, name: 'Boston' },
-          { id: 6, name: 'Columbus' },
-          { id: 7, name: 'Dallas' },
-          { id: 8, name: 'Vancouver' },
-          { id: 9, name: 'Seattle' },
-          { id: 10, name: 'Los Angeles' },
-          { id: 11, name: 'Copenhagen' },
-          { id: 12, name: 'Berlin' },
-          { id: 13, name: 'Las Vegas' },
-          { id: 14, name: 'Barcelona' },
-          { id: 15, name: 'London' },
-          { id: 16, name: 'Paris' },
-          { id: 17, name: 'Oslo' },
-          { id: 18, name: 'Malmo' }
-        ],
-          itemSelected: displayResult
-      });
-  });
+  //     $('#demo1').typeahead({
+  //         source: [
+  //         { id: 1, name: 'Toronto' },
+  //         { id: 2, name: 'Montreal' },
+  //         { id: 3, name: 'New York' },
+  //         { id: 4, name: 'Buffalo' },
+  //         { id: 5, name: 'Boston' },
+  //         { id: 6, name: 'Columbus' },
+  //         { id: 7, name: 'Dallas' },
+  //         { id: 8, name: 'Vancouver' },
+  //         { id: 9, name: 'Seattle' },
+  //         { id: 10, name: 'Los Angeles' },
+  //         { id: 11, name: 'Copenhagen' },
+  //         { id: 12, name: 'Berlin' },
+  //         { id: 13, name: 'Las Vegas' },
+  //         { id: 14, name: 'Barcelona' },
+  //         { id: 15, name: 'London' },
+  //         { id: 16, name: 'Paris' },
+  //         { id: 17, name: 'Oslo' },
+  //         { id: 18, name: 'Malmo' }
+  //       ],
+  //         itemSelected: displayResult
+  //     });
+  //     $('#demo2').typeahead({
+  //         source: [
+  //         { id: 1, name: 'Toronto' },
+  //         { id: 2, name: 'Montreal' },
+  //         { id: 3, name: 'New York' },
+  //         { id: 4, name: 'Buffalo' },
+  //         { id: 5, name: 'Boston' },
+  //         { id: 6, name: 'Columbus' },
+  //         { id: 7, name: 'Dallas' },
+  //         { id: 8, name: 'Vancouver' },
+  //         { id: 9, name: 'Seattle' },
+  //         { id: 10, name: 'Los Angeles' },
+  //         { id: 11, name: 'Copenhagen' },
+  //         { id: 12, name: 'Berlin' },
+  //         { id: 13, name: 'Las Vegas' },
+  //         { id: 14, name: 'Barcelona' },
+  //         { id: 15, name: 'London' },
+  //         { id: 16, name: 'Paris' },
+  //         { id: 17, name: 'Oslo' },
+  //         { id: 18, name: 'Malmo' }
+  //       ],
+  //         itemSelected: displayResult
+  //     });
+  //     $('#demo3').typeahead({
+  //         source: [
+  //         { id: 1, name: 'Toronto' },
+  //         { id: 2, name: 'Montreal' },
+  //         { id: 3, name: 'New York' },
+  //         { id: 4, name: 'Buffalo' },
+  //         { id: 5, name: 'Boston' },
+  //         { id: 6, name: 'Columbus' },
+  //         { id: 7, name: 'Dallas' },
+  //         { id: 8, name: 'Vancouver' },
+  //         { id: 9, name: 'Seattle' },
+  //         { id: 10, name: 'Los Angeles' },
+  //         { id: 11, name: 'Copenhagen' },
+  //         { id: 12, name: 'Berlin' },
+  //         { id: 13, name: 'Las Vegas' },
+  //         { id: 14, name: 'Barcelona' },
+  //         { id: 15, name: 'London' },
+  //         { id: 16, name: 'Paris' },
+  //         { id: 17, name: 'Oslo' },
+  //         { id: 18, name: 'Malmo' }
+  //       ],
+  //         itemSelected: displayResult
+  //     });
+  // });
 
-  $(function () {
+  // $(function () {
 
-      $('#demo4').typeahead({
-          source: [
-          { id: 1, name: 'Toronto' },
-          { id: 2, name: 'Montreal' },
-          { id: 3, name: 'New York' },
-          { id: 4, name: 'Buffalo' },
-          { id: 5, name: 'Boston' },
-          { id: 6, name: 'Columbus' },
-          { id: 7, name: 'Dallas' },
-          { id: 8, name: 'Vancouver' },
-          { id: 9, name: 'Seattle' },
-          { id: 10, name: 'Los Angeles' },
-          { id: 11, name: 'Copenhagen' },
-          { id: 12, name: 'Berlin' },
-          { id: 13, name: 'Las Vegas' },
-          { id: 14, name: 'Barcelona' },
-          { id: 15, name: 'London' },
-          { id: 16, name: 'Paris' },
-          { id: 17, name: 'Oslo' },
-          { id: 18, name: 'Malmo' }
-        ],
-          itemSelected: displayResult
-      });
-  });
+  //     $('#demo4').typeahead({
+  //         source: [
+  //         { id: 1, name: 'Toronto' },
+  //         { id: 2, name: 'Montreal' },
+  //         { id: 3, name: 'New York' },
+  //         { id: 4, name: 'Buffalo' },
+  //         { id: 5, name: 'Boston' },
+  //         { id: 6, name: 'Columbus' },
+  //         { id: 7, name: 'Dallas' },
+  //         { id: 8, name: 'Vancouver' },
+  //         { id: 9, name: 'Seattle' },
+  //         { id: 10, name: 'Los Angeles' },
+  //         { id: 11, name: 'Copenhagen' },
+  //         { id: 12, name: 'Berlin' },
+  //         { id: 13, name: 'Las Vegas' },
+  //         { id: 14, name: 'Barcelona' },
+  //         { id: 15, name: 'London' },
+  //         { id: 16, name: 'Paris' },
+  //         { id: 17, name: 'Oslo' },
+  //         { id: 18, name: 'Malmo' }
+  //       ],
+  //         itemSelected: displayResult
+  //     });
+  // });
 
   // search destination 
   var substringMatcher = function(strs) {
@@ -271,14 +294,8 @@
       cb(matches);
     };
   };
-  // datepicker
-  $(function () {
-    // departure date picker
-    $('#depart').datepicker(); // departure date picker
-    $('#departOneWay').datepicker();
-    // arrival date picker
-    $('#arriveDate').datepicker();
-    })
+  
+
 
   // results go back 
   $("#backToSearchBtn").click(function(){
