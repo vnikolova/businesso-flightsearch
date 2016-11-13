af8e3cc341066ad50f0ad1180aa650e9ca7714ca
vnikolova <viksnikolova@gmail.com>
2016-11-13 21:30:36 +0100

    display preferences from db

--- js/profile.js
+++ js/profile.js
@@ -2,14 +2,34 @@ $(document).ready ( function(){
 
   $.getJSON('php/get-profile.php',function(data)
   {
-    console.log(data);
+    console.log(data[0]);
     //display personal info
-    // $('#').text(objVal);
+    $('#firstName span').text(data[0].firstname);
+    $('#lastName span').text(data[0].lastname);
+    $('#email span').text(data[0].email);
+    $('#mobile span').text(data[0].mobile);
+    $('#passportID span').text(data[0].passpord_ID);
+
+    //destination and time
+    $('#time span').text(data[0].departure_Time);
+    $('#departure span').text(data[0].departure);
 
     //display accommodation
+    $('#accommoType span').text(data[0].accommo_Type);
+    $('#rooms span').text(data[0].accommo_Rooms);
+    $('#price span').text('from '+data[0].accommo_PriceMin +' to '+data[0].accommo_PriceMax);
 
     //display services
+    $('#checkedBaggage span').text(data[0].baggage_Checked);
+    $('#cabinBaggage span').text(data[0].baggage_Cabin);
+    $('#seat span').text(data[0].seat);
+    $('#meal span').text(data[0].meal);
 
     //display payment
+    $('#cardNum span').text(data[0].card_Num);
+    $('#expDate span').text(data[0].card_Date);
+    $('#cardholder span').text(data[0].card_name);
+    $('#ccv span').text(data[0].card_ccv);
+
   });
 });
--- profile.php
+++ profile.php
@@ -50,33 +50,33 @@ if(isset($_SESSION['username'])) {
       <!-- column one - personal info , flight destination, accomo -->
       <div class="col-xs-5 col-xs-offset-1">
         <h3>Pesonal Information</h3>
-        <h4 id="firstName"><small>First Name</small></h4>
-        <h4 id="lastName"><small>Last Name</small></h4>
-        <h4 id="email"><small>E-mail</small></h4>
-        <h4 id="mobile"><small>Mobile</small></h4>
-        <h4 id="passportID"><small>Passport / ID</small></h4>
+        <h4 id="firstName"><small>First Name</small><span></span></h5>
+        <h4 id="lastName"><small>Last Name</small><span></span></h4>
+        <h4 id="email"><small>E-mail</small><span></span></h4>
+        <h4 id="mobile"><small>Mobile</small><span></span></h4>
+        <h4 id="passportID"><small>Passport / ID</small><span></span></h4>
         <h3>Destination and Time</h3>
-        <h4 id="time"><small>Time</small></h4>
-        <h4 id="departure"><small>Departing from</small></h4>
+        <h4 id="time"><small>Time</small><span></span></h4>
+        <h4 id="departure"><small>Departing from</small><span></span></h4>
         <h3>Accommodation</h3>
-        <h4 id="accommoType"><small>Type</small></h4>
-        <h4 id="rooms"><small>Rooms</small></h4>
-        <h4 id="price"><small>Price Range</small></h4>
+        <h4 id="accommoType"><small>Type</small><span></span></h4>
+        <h4 id="rooms"><small>Rooms</small><span></span></h4>
+        <h4 id="price"><small>Price Range</small><span></span></h4>
 
       </div>
 
       <!-- column two - service and payment info -->
       <div class="col-xs-5">
         <h3>Services</h3>
-        <h4 id="checkedBaggage"><small>Checked-in Baggage</small></h4>
-        <h4 id="cabinBaggage"><small>Cabin Baggage</small></h4>
-        <h4 id="seat"><small>Seat</small></h4>
-        <h4 id="meal"><small>Meal</small></h4>
+        <h4 id="checkedBaggage"><small>Checked-in Baggage</small><span></span></h4>
+        <h4 id="cabinBaggage"><small>Cabin Baggage</small><span></span></h4>
+        <h4 id="seat"><small>Seat</small><span></span></h4>
+        <h4 id="meal"><small>Meal</small><span></span></h4>
         <h3>Payment</h3>
-        <h4 id="cardNum"><small>Card</small></h4>
-        <h4 id="expDate"><small>Expiry Date</small></h4>
-        <h4 id="cardholder"><small>Cardholder</small></h4>
-        <h4 id="ccv"><small>CCV</small></h4>
+        <h4 id="cardNum"><small>Card</small><span></span></h4>
+        <h4 id="expDate"><small>Expiry Date</small><span></span></h4>
+        <h4 id="cardholder"><small>Cardholder</small><span></span></h4>
+        <h4 id="ccv"><small>CCV</small><span></span></h4>
 
       </div>
     </div>
